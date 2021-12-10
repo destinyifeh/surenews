51dcdc2aa7cbcc57754f0d39ebaaea79ec2abaa5
destinyifeh <destinyifeh@gmail.com>
2021-12-10 08:45:10 -1200

    updated

--- client/sitemap-generator.js
+++ client/sitemap-generator.js
@@ -9,7 +9,7 @@ const Sitemap = require('react-router-sitemap').default;
 function generateSitemap(){
     return(
         new Sitemap(router)
-        .build('/')
+        .build('http://localhost:3000')
         .save('./public/sitemap.xml')
     );
 }
